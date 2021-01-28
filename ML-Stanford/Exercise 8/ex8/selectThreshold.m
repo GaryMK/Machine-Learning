15e1e4ae7e015f65bb777b61f076e6f34d9200ff
GaryMK <49356298+GaryMK@users.noreply.github.com>
2021-01-28 19:31:07 +0800

    added Anomaly Detection

--- ML-Stanford/Exercise 8/ex8/selectThreshold.m
+++ ML-Stanford/Exercise 8/ex8/selectThreshold.m
@@ -25,13 +25,13 @@ for epsilon = min(pval):stepsize:max(pval)
 
 
 
-
-
-
-
-
-
-
+    predictions = (pval < epsilon);
+    tp = sum((predictions == 1) & (yval == 1));
+    fp = sum((predictions == 1) & (yval == 0));
+    fn = sum((predictions == 0) & (yval == 1));
+    precision = tp / (tp + fp);
+    recall = tp / (tp + fn);
+    F1 = (2 * precision * recall) / (precision + recall);
 
 
 
